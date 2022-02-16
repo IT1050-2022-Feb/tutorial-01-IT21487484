ddc14525b1d27a090ff0363b3d262370a0467379
IT21487484 <99793125+IT21487484@users.noreply.github.com>
2022-02-16 23:38:03 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,22 @@
 #include <stdio.h>
 
 int main() {
-  
+   
+   int marks1 , marks2;
+	float average;
+	
+	printf("Enter marks of subject 1: ");
+	scanf("%d" , &marks1);
+   
+   printf("Enter marks of subject 2: ");
+	scanf("%d" , &marks2);
+	
+	average = (float) (marks1 + marks2) / 2;
+	
+	printf("\n");
+	printf("The Average of the 2 marks is : %.2f\n" , average);
+	
   return 0;
+   
 }
 
